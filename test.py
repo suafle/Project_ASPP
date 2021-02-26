1209ebef5b16dd7f439a24ef588e5f7cfe933a3e
suafle <matias.suazo@ug.uchile.cl>
2021-02-26 19:17:37 +0100

    Test.py file update, time added

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 import functions.normal.BB as BB
 import functions.cython.BB as c_BB
 import numpy as np
+import time
 
 W1 = np.array([0])
 TDS = 300
 LStar = np.array([1])
 eta = 0.9
 
-print(BB.W1_DS(W1,TDS,LStar,eta))
-print(c_BB.W1_DS(W1,TDS,LStar,eta))
+t0 = time.time()
+BB.W1_DS(W1,TDS,LStar,eta)
+print(time.time()-t0)
+t0 = time.time()
+c_BB.W1_DS(W1,TDS,LStar,eta)
+print(time.time()-t0)
